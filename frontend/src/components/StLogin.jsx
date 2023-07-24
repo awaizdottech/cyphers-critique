016ca11301a40e96facef5287d68fd35ecad5265
ALPHA25742 <160420737082@mjcollege.ac.in>
2023-07-24 20:21:47 +0530

    shut up errors

--- frontend/src/components/StLogin.jsx
+++ frontend/src/components/StLogin.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+import { useNavigate } from 'react-router-dom';
 
 function StLogin() {
   const [email, setEmail] = useState("");
